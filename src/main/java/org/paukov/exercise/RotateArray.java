42f44d458486f8a7b1688186a0fee933e20e5a91
dpaukov <d.paukov@gmail.com>
2015-05-04 14:07:21 -0700

    Add implementation of an array rotate algorithm

---
+++ src/main/java/org/paukov/exercise/RotateArray.java
@@ -0,0 +1,31 @@
+package org.paukov.exercise;
+
+/**
+ * Created by dpaukov
+ */
+public class RotateArray {
+
+    public void rotate(int[] array, int k){
+        if (k<=0){
+            return;
+        }
+        k = k % array.length;
+
+        reverse(array, array.length-k, array.length-1);
+        reverse(array, 0, array.length-k-1);
+        reverse(array, 0, array.length-1);
+    }
+
+    void reverse(int[] array, int start, int end){
+        int length = (end - start) / 2;
+        for (int i = 0; i <= length; i++){
+            swap(array, start + i, end - i);
+        }
+    }
+
+    void swap(int[] array, int start, int end){
+        int value = array[start];
+        array[start]=array[end];
+        array[end]=value;
+    }
+}
---
+++ src/test/java/org/paukov/exercise/RotateArrayTest.java
@@ -0,0 +1,67 @@
+package org.paukov.exercise;
+
+import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+/**
+ * Created by dpaukov
+ */
+public class RotateArrayTest {
+
+    @Test
+    public void test_reverse_1(){
+        RotateArray rotateArray = new RotateArray();
+        int[] array = new int[] {1,2,3,4,5,6,7,8,9,10};
+        rotateArray.reverse(array, 0, 7);
+        assertThat(array).isEqualTo(new int[]{8,7,6,5,4,3,2,1,9,10});
+    }
+
+    @Test
+    public void test_reverse_2(){
+        RotateArray rotateArray = new RotateArray();
+        int[] array = new int[] {1,2,3,4,5,6,7,8,9,10};
+        rotateArray.reverse(array, 0, 6);
+        assertThat(array).isEqualTo(new int[]{7,6,5,4,3,2,1,8,9,10});
+    }
+
+    @Test
+    public void test_rotate_1(){
+        RotateArray rotateArray = new RotateArray();
+        int[] array = new int[] {1,2,3,4,5,6,7,8,9,10,11};
+        rotateArray.rotate(array, 2);
+        assertThat(array).isEqualTo(new int[]{10,11,1,2,3,4,5,6,7,8,9});
+    }
+
+    @Test
+    public void test_rotate_2(){
+        RotateArray rotateArray = new RotateArray();
+        int[] array = new int[] {1,2,3,4,5,6,7,8,9,10};
+        rotateArray.rotate(array, 2);
+        assertThat(array).isEqualTo(new int[]{9,10,1,2,3,4,5,6,7,8});
+    }
+
+    @Test
+    public void test_rotate_3(){
+        RotateArray rotateArray = new RotateArray();
+        int[] array = new int[] {1,2,3,4,5,6,7,8,9,10};
+        rotateArray.rotate(array, 6);
+        assertThat(array).isEqualTo(new int[]{5,6,7,8,9,10,1,2,3,4});
+    }
+
+    @Test
+    public void test_rotate_4(){
+        RotateArray rotateArray = new RotateArray();
+        int[] array = new int[] {1,2,3,4,5,6,7,8,9,10,11};
+        rotateArray.rotate(array, 0);
+        assertThat(array).isEqualTo(new int[]{1,2,3,4,5,6,7,8,9,10,11});
+    }
+
+    @Test
+    public void test_rotate_5(){
+        RotateArray rotateArray = new RotateArray();
+        int[] array = new int[] {1,2,3,4,5,6,7,8,9,10,11};
+        rotateArray.rotate(array, -2);
+        assertThat(array).isEqualTo(new int[]{1,2,3,4,5,6,7,8,9,10,11});
+    }
+}
