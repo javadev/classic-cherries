c325fa8c54fff5aa1ee2e509889894479190cda9
dpaukov <d.paukov@gmail.com>
2018-03-04 23:46:39 -0800

    Add backtracking abstract skeleton class

---
+++ src/main/java/org/paukov/backtracking/Backtracking.java
@@ -0,0 +1,36 @@
+package org.paukov.backtracking;
+
+import java.util.List;
+
+/**
+ * Created by dpaukov on 3/4/18.
+ */
+public abstract class Backtracking<T, I> {
+
+    protected boolean finished = false;
+
+    protected abstract boolean isSolution(T[] vector, int k, I dataInput);
+
+    protected abstract void processSolution(T[] vector, int k, I dataInput);
+
+    protected abstract List<T> constructCandidates(T[] vector, int k, I dataInput);
+
+    protected abstract void makeMove(T[] vector, int k, I dataInput);
+
+    protected abstract void unmakeMove(T[] vector, int k, I dataInput);
+
+    public void run(T[] vector, int k, I dataInput) {
+        if (isSolution(vector, k, dataInput)) {
+            processSolution(vector, k, dataInput);
+        } else {
+            for (T c : constructCandidates(vector, k, dataInput)) {
+                vector[k + 1] = c;
+                makeMove(vector, k + 1, dataInput);
+                run(vector, k + 1, dataInput);
+                unmakeMove(vector, k + 1, dataInput);
+                if (finished) return;
+            }
+
+        }
+    }
+}
