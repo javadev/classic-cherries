4407c64b51677ecc9febdcb8e776710a9d96513f
dpaukov <d.paukov@gmail.com>
2018-03-10 22:54:33 -0800

    Unit tests to MaxPossibleTime

---
+++ src/test/java/org/paukov/backtracking/MaxPossibleTimeTest.java
@@ -0,0 +1,40 @@
+package org.paukov.backtracking;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+/**
+ * Created by dpaukov on 3/10/18.
+ */
+public class MaxPossibleTimeTest {
+    @Test
+    public void calc_1925() throws Exception {
+        MaxPossibleTime maxPossibleTime = new MaxPossibleTime();
+        String time = maxPossibleTime.calc(Arrays.asList(1, 9, 2, 5));
+        assertThat(time).isEqualTo("21:59");
+    }
+
+    @Test
+    public void calc_3925() throws Exception {
+        MaxPossibleTime maxPossibleTime = new MaxPossibleTime();
+        String time = maxPossibleTime.calc(Arrays.asList(3, 9, 2, 5));
+        assertThat(time).isEqualTo("23:59");
+    }
+
+    @Test
+    public void calc_1277() throws Exception {
+        MaxPossibleTime maxPossibleTime = new MaxPossibleTime();
+        String time = maxPossibleTime.calc(Arrays.asList(1, 2, 7, 7));
+        assertThat(time).isEqualTo("17:27");
+    }
+
+    @Test
+    public void calc_3277() throws Exception {
+        MaxPossibleTime maxPossibleTime = new MaxPossibleTime();
+        String time = maxPossibleTime.calc(Arrays.asList(3, 2, 7, 7));
+        assertThat(time).isNull();
+    }
+}
