7d9a8fd7de31f48a91587d92b7a9ee343e540133
dpaukov <d.paukov@gmail.com>
2018-03-18 22:59:05 -0700

    Solution for the majority element problem.
    Given an array of size n, find the majority element. The majority element
    is the element that appears more than ⌊n/2⌋ times. You may assume that the array
    is non-empty and the majority element always exist in the array.
    
    https://leetcode.com/problems/majority-element/

---
+++ src/main/java/org/paukov/exercise/bits/MajorityElement.java
@@ -0,0 +1,32 @@
+package org.paukov.exercise.bits;
+
+/**
+ * Created by dpaukov on 3/18/18.
+ * Given an array of size n, find the majority element. The majority element
+ * is the element that appears more than ⌊ n/2 ⌋ times.
+ * You may assume that the array is non-empty and the majority element
+ * always exist in the array.
+ * <p>
+ * https://leetcode.com/problems/majority-element/
+ */
+public class MajorityElement {
+
+    // Solution based on Boyer–Moore majority vote algorithm
+    // https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore_majority_vote_algorithm
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = nums[0];
+        for (int c : nums) {
+            if (candidate == c) {
+                count++;
+            } else {
+                if (count == 0) {
+                    candidate = c;
+                } else {
+                    count--;
+                }
+            }
+        }
+        return candidate;
+    }
+}
