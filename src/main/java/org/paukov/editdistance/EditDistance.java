1178656e03cbc7ba8075d411b627e4b3ec69aafd
dpaukov <d.paukov@gmail.com>
2018-03-22 21:24:14 -0700

    Added description for the EditDistance class

--- src/main/java/org/paukov/editdistance/EditDistance.java
+++ src/main/java/org/paukov/editdistance/EditDistance.java
@@ -2,6 +2,17 @@ package org.paukov.editdistance;
 
 /**
  * Created by dpaukov on 3/22/18.
+ * <p>
+ * Given two words word1 and word2, find the minimum number of steps required to convert word1 to word2.
+ * (each operation is counted as 1 step.)
+ * <p>
+ * You have the following 3 operations permitted on a word:
+ * <p>
+ * a) Insert a character
+ * b) Delete a character
+ * c) Substitute a character
+ * <p>
+ * https://leetcode.com/problems/edit-distance/description/
  */
 public class EditDistance {
 
