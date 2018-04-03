333ecd4fb4daa5016f739adf7ac5eb23d4926eb1
dpaukov <d.paukov@gmail.com>
2018-04-03 00:13:27 -0700

    Add solution for the integer partition problem of splitting a multiset of integer values into two parts with the same sum.

---
+++ src/main/java/org/paukov/knapsack/PartitionProblem.java
@@ -0,0 +1,62 @@
+package org.paukov.knapsack;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by dpaukov on 4/2/18.
+ * <p>
+ * Splits an array of integer values into 2 part of the same sum.
+ */
+public class PartitionProblem {
+
+    public static List<List<Integer>> calculate(Integer[] array) {
+        int n = array.length;
+        int sum = 0;
+        for (Integer value : array) {
+            sum += value;
+        }
+        int k = sum / 2;
+        boolean[][] m = new boolean[k + 1][n + 1];
+
+        for (int j = 0; j <= n; j++) {
+            m[0][j] = true;
+        }
+
+        for (int i = 1; i <= k; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i >= array[j - 1]) {
+                    m[i][j] = m[i][j - 1] || m[i - array[j - 1]][j - 1];
+                } else {
+                    m[i][j] = m[i][j - 1];
+                }
+            }
+        }
+
+        System.out.println("Partition Exists: " + m[k][n] + ", array: " + Arrays.toString(array) + " sum: " + sum);
+
+        if (m[k][n]) {
+            List<Integer> firstPartition = new ArrayList<>();
+            List<Integer> secondPartition = new ArrayList<>(Arrays.asList(array));
+
+            int r = k;
+            int t = n;
+            while (r > 0 && t > 0) {
+                while (m[r][t]) {
+                    t--;
+                }
+                firstPartition.add(array[t]);
+                secondPartition.remove(array[t]);
+                r = r - array[t];
+            }
+            return Arrays.asList(firstPartition, secondPartition);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(PartitionProblem.calculate(new Integer[]{1, 2, 3, 4, 5, 6, 7, 8}));
+    }
+
+}
---
+++ src/test/java/org/paukov/knapsack/PartitionProblemTest.java
@@ -0,0 +1,33 @@
+package org.paukov.knapsack;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+/**
+ * Created by dpaukov on 4/3/18.
+ */
+public class PartitionProblemTest {
+    @Test
+    public void calculate() {
+        assertThat(PartitionProblem.calculate(new Integer[]{3, 1, 1, 2, 2, 1}))
+                .containsExactly(Arrays.asList(1, 1, 3), Arrays.asList(2, 2, 1));
+
+        assertThat(PartitionProblem.calculate(new Integer[]{3, 10, 1, 2, 2, 1, 4, 5}))
+                .containsExactly(Arrays.asList(1, 10, 3), Arrays.asList(2, 2, 1, 4, 5));
+
+        assertThat(PartitionProblem.calculate(new Integer[]{1, 2, 3, 4, 5, 6, 7, 8}))
+                .containsExactly(Arrays.asList(6, 5, 4, 2, 1), Arrays.asList(3, 7, 8));
+
+        assertThat(PartitionProblem.calculate(new Integer[]{1, 20, 18, 2, 30, 10, 15, 15, 4}))
+                .containsExactly(Arrays.asList(15, 10, 30, 2), Arrays.asList(1, 20, 18, 15, 4));
+    }
+
+    @Test
+    public void calculate_no_partition() {
+        assertThat(PartitionProblem.calculate(new Integer[]{1, 1, 5})).isNull();
+    }
+
+}
