97dfa9635767925387cfc52996b17ea58735fbbd
dpaukov <d.paukov@gmail.com>
2018-04-07 21:33:43 -0700

    Add unit tests for Graph traversal algorithms BSF and DSF.

---
+++ src/test/java/org/paukov/graph/GraphTest.java
@@ -0,0 +1,41 @@
+package org.paukov.graph;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+/**
+ * Created by dpaukov on 4/7/18.
+ */
+public class GraphTest {
+    @Test
+    public void bfs() throws Exception {
+        Graph graph = new Graph(6);
+        graph.addBiDirectedEdge(0, 1);
+        graph.addBiDirectedEdge(0, 4);
+        graph.addBiDirectedEdge(0, 5);
+        graph.addBiDirectedEdge(1, 2);
+        graph.addBiDirectedEdge(1, 4);
+        graph.addBiDirectedEdge(2, 3);
+        graph.addBiDirectedEdge(3, 4);
+        List<Integer> bsfTraversalNodes = graph.bfs(0);
+        assertThat(bsfTraversalNodes).containsExactly(0, 1, 4, 5, 2, 3);
+
+    }
+
+    @Test
+    public void dfs() throws Exception {
+        Graph graph = new Graph(6);
+        graph.addBiDirectedEdge(0, 1);
+        graph.addBiDirectedEdge(0, 4);
+        graph.addBiDirectedEdge(0, 5);
+        graph.addBiDirectedEdge(1, 2);
+        graph.addBiDirectedEdge(1, 4);
+        graph.addBiDirectedEdge(2, 3);
+        graph.addBiDirectedEdge(3, 4);
+        List<Integer> dsfTraversalNodes = graph.dfs(0);
+        assertThat(dsfTraversalNodes).containsExactly(0, 1, 2, 3, 4, 5);
+    }
+}
