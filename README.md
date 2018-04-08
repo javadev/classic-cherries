00686b5f3311bf4c4ce1b2a5973885de84efc7df
dpaukov <d.paukov@gmail.com>
2018-04-07 23:19:10 -0700

    Refactoring the Graph class, removing unused methods.

--- README.md
+++ README.md
@@ -11,4 +11,4 @@ Implementation of some classical computer science algorithms
 - Backtracking (for generating subsets, permutations, solving sudoku, etc.)
 - Dynamic Programming (for calculating edit distance, longest common subsequence, etc)
 - 0/1 Knapsack Problem, Integer Partition (Multiset of Integers) Problem
-- Graphs (BSF, DSF, all connected components, finding path)
+- Graphs (BSF, DSF, all connected components, finding paths, finding cycles)
--- src/main/java/org/paukov/graph/Graph.java
+++ src/main/java/org/paukov/graph/Graph.java
@@ -5,7 +5,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.function.BiConsumer;
-import java.util.function.IntConsumer;
 
 /**
  * Created by dpaukov on 4/7/18.
@@ -17,37 +16,6 @@ public class Graph {
     final int size;
     final List<Integer>[] edges;
     int time = 0;
-
-    public Graph(int size) {
-        this.size = size;
-        this.edges = new List[size];
-        for (int i = 0; i < size; i++) {
-            this.edges[i] = new ArrayList<>();
-        }
-    }
-
-    public void addEdge(int from, int to) {
-        assert from < this.size;
-        assert to < this.size;
-        this.edges[from].add(to);
-    }
-
-    public void addBiDirectedEdge(int from, int to) {
-        assert from < this.size;
-        assert to < this.size;
-        this.edges[from].add(to);
-        this.edges[to].add(from);
-    }
-
-    public void addEdges(int[] edges) {
-        for (int i = 0; i < edges.length - 1; i++) {
-            int from = edges[i];
-            int to = edges[i + 1];
-            assert from < this.size;
-            assert to < this.size;
-            this.edges[from].add(to);
-        }
-    }
 
     static class NodeInfo {
         boolean discovered;
@@ -77,6 +45,27 @@ public class Graph {
         static Edge of(int i, int j) {
             return new Edge(i, j);
         }
+    }
+
+    public Graph(int size) {
+        this.size = size;
+        this.edges = new List[size];
+        for (int i = 0; i < size; i++) {
+            this.edges[i] = new ArrayList<>();
+        }
+    }
+
+    public void addEdge(int from, int to) {
+        assert from < this.size;
+        assert to < this.size;
+        this.edges[from].add(to);
+    }
+
+    public void addBiDirectedEdge(int from, int to) {
+        assert from < this.size;
+        assert to < this.size;
+        this.edges[from].add(to);
+        this.edges[to].add(from);
     }
 
     public List<Integer> bfs(int root) {
@@ -125,12 +114,12 @@ public class Graph {
         List<Integer> list = new ArrayList<>();
         dfs(root,
                 NodeInfo.build(size),
-                (v) -> {
+                (v, info) -> {
                     System.out.println("Process vertex: " + v);
                     list.add(v);
                 },
-                (i, j) -> System.out.println("Process edge: " + i + "," + j),
-                (v) -> {
+                (edge, info) -> System.out.println("Process edge: " + edge.i + "," + edge.j),
+                (v, info) -> {
                     // do nothing here
                 });
         return list;
@@ -139,26 +128,26 @@ public class Graph {
 
     public void dfs(int node,
                     NodeInfo[] info,
-                    IntConsumer processNodeBefore,
-                    BiConsumer<Integer, Integer> processEdge,
-                    IntConsumer processNodeAfter) {
+                    BiConsumer<Integer, NodeInfo[]> processNodeBefore,
+                    BiConsumer<Edge, NodeInfo[]> processEdge,
+                    BiConsumer<Integer, NodeInfo[]> processNodeAfter) {
         assert node < this.size;
         info[node].discovered = true;
         time = time + 1;
         info[node].entryTime = time;
 
-        processNodeBefore.accept(node);
+        processNodeBefore.accept(node, info);
 
         for (Integer child : edges[node]) {
             if (!info[child].discovered) {
                 info[child].parent = node;
-                processEdge.accept(node, child);
+                processEdge.accept(Edge.of(node, child), info);
                 dfs(child, info, processNodeBefore, processEdge, processNodeAfter);
             } else if (!info[child].processed) {
-                processEdge.accept(node, child);
+                processEdge.accept(Edge.of(node, child), info);
             }
         }
-        processNodeAfter.accept(node);
+        processNodeAfter.accept(node, info);
         time = time + 1;
         info[node].exitTime = time;
         info[node].processed = true;
