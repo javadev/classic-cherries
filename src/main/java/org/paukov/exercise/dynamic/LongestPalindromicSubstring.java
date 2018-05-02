c101974bee8c6855cb862e7d9ba61b071c32b6d8
dpaukov <d.paukov@gmail.com>
2018-05-01 17:45:54 -0700

    Add solution for Longest Palindromic Substring Problem #5 https://leetcode.com/problems/longest-palindromic-substring/description/

---
+++ src/main/java/org/paukov/exercise/dynamic/LongestPalindromicSubstring.java
@@ -0,0 +1,65 @@
+package org.paukov.exercise.dynamic;
+
+/**
+ * Created by dpaukov on 5/1/18.
+ * <p>
+ * 5. Longest Palindromic Substring
+ * <p>
+ * https://leetcode.com/problems/longest-palindromic-substring/description/
+ * <p>
+ * Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: "babad"
+ * Output: "bab"
+ * Note: "aba" is also a valid answer.
+ * Example 2:
+ * <p>
+ * Input: "cbbd"
+ * Output: "bb"
+ */
+public class LongestPalindromicSubstring {
+
+    /**
+     * Idea of this DP solution: P[i][j] = P[i+1][j-1] && s[i]==s[j];
+     */
+    public static String longestPalindrome(String s) {
+        int n = s.length();
+        boolean[][] p = new boolean[n][n];
+
+        for (int k = 0; k < n; k++) {
+            int j = k;
+            int i = 0;
+            while ((j < n) && (i < n - k)) {
+                if (i == j) {
+                    p[i][j] = true;
+                } else if (i + 1 == j) {
+                    p[i][j] = s.charAt(i) == s.charAt(j);
+                } else {
+                    p[i][j] = p[i + 1][j - 1] && (s.charAt(i) == s.charAt(j));
+                }
+                j++;
+                i++;
+            }
+        }
+
+        // search the longest
+        for (int k = n - 1; k >= 0; k--) {
+            int j = k;
+            int i = 0;
+            while ((j < n) && (i < n - k)) {
+                if (p[i][j]) {
+                    return s.substring(i, j + 1);
+                }
+                j++;
+                i++;
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Longest Palindrome: " + longestPalindrome("asdfabwbalknbd"));
+    }
+}
