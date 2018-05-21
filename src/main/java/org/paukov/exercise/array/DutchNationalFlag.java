43768ae114bc69624c55dc54c94301f448d63664
dpaukov <d.paukov@gmail.com>
2018-05-21 00:19:51 -0700

    Add solution for partitioning array with values one of four (from EPI).

--- src/main/java/org/paukov/exercise/array/DutchNationalFlag.java
+++ src/main/java/org/paukov/exercise/array/DutchNationalFlag.java
@@ -38,7 +38,38 @@ public class DutchNationalFlag {
     public static void main(String args[]) {
         System.out.println("Partition: " + Arrays.toString(partition(new int[]{1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2}, 2)));
         System.out.println("Partition: " + Arrays.toString(partition(new int[]{0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0}, 0)));
+
+        System.out.println("Partition4: " + Arrays.toString(partition4(new int[]{1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3})));
     }
 
+    // Given an array of objects with keys that takes only one of 4 values
+    // reorder it so all objects with the same key appear together.
+    static int[] partition4(int[] array) {
+        int lt = 0;
+        int gt = array.length - 1;
+        int i = 0;
+        while (i <= gt) {
+            if (array[i] < 2) {
+                swap(array, i, lt);
+                i++;
+                lt++;
+            } else if (array[i] > 3) {
+                swap(array, i, gt);
+                gt--;
+            } else {
+                i++;
+            }
+        }
+        i = lt;
+        while (i <= gt) {
+            if (array[i] == 3) {
+                swap(array, i, gt);
+                gt--;
+            } else {
+                i++;
+            }
+        }
+        return array;
+    }
 
 }
